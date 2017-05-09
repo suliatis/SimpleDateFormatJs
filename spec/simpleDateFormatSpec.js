6bfbc81906b4ee08160a3d5578295bf725705a49
Attila Süli <suli.zakar.attila@gmail.com>
2017-05-09 14:18:15 -0400

    Initial commit.

---
+++ spec/simpleDateFormatSpec.js
@@ -0,0 +1,6 @@
+var sdf = require("../src/simpleDateFormat.js");
+describe("Hello World", function () {
+	it("is a simple test case", function() {
+		expect(sdf.sayHello()).toBe("hello");
+	});
+});
